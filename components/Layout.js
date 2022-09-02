1ed9911088459ef57cc31636a0478acbb685c915
Pavan Hegde <pavanrkadave@gmail.com>
2022-09-02 21:03:31 +0200

    Add Page title

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,13 @@
 import Navbar from "./core/Navbar";
 import { home } from "../data";
+import Head from "next/head";
 
 const Layout = ({ children }) => (
   <div>
+    <Head>
+      <title>Pavan Hegde</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <Navbar />
     {children}
   </div>
