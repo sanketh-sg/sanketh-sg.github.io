eb41ba51c01cd4bd8c14662c1dd87bd66de0e914
Pavan Hegde <pavanrkadave@gmail.com>
2022-09-02 20:31:21 +0200

    fix key

--- components/containers/about.js
+++ components/containers/about.js
@@ -1,62 +1,64 @@
-import React from 'react';
-import { about } from '../../data';
-import ReactTooltip from 'react-tooltip';
+import React from "react";
+import ReactTooltip from "react-tooltip";
+import Image from "next/image";
+import { about } from "../../data";
 
 export default function About() {
-    return (
-        <div>
-            <div className="bg-container">
-                <section className="about">
-                    <div className="content">
-                        <h1>
-                            Know Who <span> I Am </span>
-                        </h1>
-                        <p>{about.aboutMe}</p>
+  return (
+    <div>
+      <div className="bg-container">
+        <section className="about">
+          <div className="content">
+            <h1>
+              Know Who <span> I Am </span>
+            </h1>
+            <p>{about.aboutMe}</p>
 
-                        <ul>
-                            {about.hobbies.map((hobby) => (
-                                <li>
-                                    <i className="fa fa-angle-right"></i>{' '}
-                                    {hobby}
-                                </li>
-                            ))}
-                        </ul>
+            <ul>
+              {about.hobbies.map((hobby, index) => (
+                <li key={index}>
+                  <i className="fa fa-angle-right"></i> {hobby}
+                </li>
+              ))}
+            </ul>
 
-                        <span className="quote">"{about.quote}"</span>
-                    </div>
+            <span className="quote">&quot;{about.quote}&quot;</span>
+          </div>
 
-                    <img src="assets/coding.svg" alt="" />
-                </section>
-            </div>
-            <section className="skillset">
-                <div className="container">
-                    <ReactTooltip place="top" type="light" effect="solid" />
-                    <h1>
-                        Technical <span>Skillset</span>
-                    </h1>
-                    <div className="icons-container">
-                        {about.skills.map((skill) => (
-                            <i
-                                data-tip={skill}
-                                className={`devicon-${skill}-plain colored`}
-                            ></i>
-                        ))}
-                    </div>
-                </div>
-                <div className="container">
-                    <h1>
-                        <span>Tools</span> I use
-                    </h1>
-                    <div className="icons-container tools">
-                        {about.tools.map((tool) => (
-                            <i
-                                data-tip={tool}
-                                className={`devicon-${tool}-plain colored`}
-                            ></i>
-                        ))}
-                    </div>
-                </div>
-            </section>
+          <img src="./assets/coding.svg" alt="" />
+        </section>
+      </div>
+      <section className="skillset">
+        <div className="container">
+          <ReactTooltip place="top" type="light" effect="solid" />
+          <h1>
+            Technical <span>Skillset</span>
+          </h1>
+          <div className="icons-container">
+            {about.skills.map((skill, index) => (
+              <i
+                key={index}
+                data-tip={skill}
+                className={`devicon-${skill}-plain colored`}
+              ></i>
+            ))}
+          </div>
         </div>
-    );
+        <div className="container">
+          <h1>
+            <span>Tools</span> I use
+          </h1>
+          <div className="icons-container tools">
+            {about.tools.map((tool, index) => (
+              <i
+                key={index}
+                data-tip={tool}
+                className={`devicon-${tool}-plain colored`}
+              ></i>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
